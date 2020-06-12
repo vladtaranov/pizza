ea6cdaceca737703261101fe5c06613d7522e9e6
Vladislav Taranov <vladislav.taranov@live.com>
2020-06-12 14:46:24 +0300

    Validate value utils addition

---
+++ src/client/utils/validate.js
@@ -0,0 +1,13 @@
+const validateValue = (value, regExp) => {
+  let validatedValue = '';
+  for (const char of value) {
+    if (char.match(regExp)) {
+      validatedValue += char;
+    }
+  }
+  return validatedValue;
+};
+
+export default {
+  validateValue
+};
